c281a97d0263b8c8460fdbafad90970dd4fdd993
Nguyen Minh Huy Duong <minhhuy1411417@gmail.com>
2022-12-18 02:15:53 -0800

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Nice
+          I was changed on the feature brach
         </p>
       </header>
     </div>
